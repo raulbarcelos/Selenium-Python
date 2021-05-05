e7cbc028d41de31d03312bf2015cbda8fb572449
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-05 20:00:57 -0300

    Adicionando try except na funcionalidade realizar_login()
    Realizando o tratamento de erros com o try except.

--- login.py
+++ login.py
@@ -16,27 +16,34 @@ driver.get(usuario.url)
 
 
 def realizar_login():
-    # carregar elemento do campo input do formulário (email)
-    login_email = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "login_email"))
-    )
-    login_email.click()
-    email = usuario.email
-    login_email.send_keys(email)  # Enviar key email
+    try:
+        # carregar elemento do campo input do formulário (email)
+        login_email = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "login_email"))
+        )
+        login_email.click()
+        email = usuario.email
+        login_email.send_keys(email)  # Enviar key email
 
-    #carregar elemento do campo formulário de preencher senha
-    login_senha = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "login_senha"))
-    )
-    login_senha.click()
-    senha = usuario.senha
-    login_senha.send_keys(senha)  # Enviar key password
+        #carregar elemento do campo formulário de preencher senha
+        login_senha = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "login_senha"))
+        )
+        login_senha.click()
+        senha = usuario.senha
+        login_senha.send_keys(senha)  # Enviar key password
 
-    # carregar elemento do botão de login
-    login_form = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//button[@class='ui-button -warning -medium -block--sm']"))
-    )
-    login_form.click()  # Clicar no botão login
+        # carregar elemento do botão de login
+        login_form = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@class='ui-button -warning -medium -block--sm']"))
+        )
+        login_form.click()  # Clicar no botão login
+    
+    except:
+        print("Erro ao realizar login!")
+        driver.quit()
+    finally:
+        print("Login realizado!")
 
 
 realizar_login()
